93b31b911cdb39cc6e8f823c488e696a2c32db79
pstance <pstanton15@gmail.com>
2018-11-30 10:32:58 -0500

    Fixed Collision Check
    I updated collision check such that it only takes two angles. The angle in the x-y plane gives us the change in x and y whereas the y-z angle simply gives us the change in z.

--- collision.py
+++ collision.py
@@ -22,14 +22,13 @@ def within_model_bounds(position):
 #	   : Orientation defined by (theta, alpha, and beta) in radians
 #      		: theta is defined as the angle of the line in the x-y plane 
 #      		: alpha in the y-z plane
-#	   		: beta in the x-z plane
 #      : Dictionary of buildings with x,y,z ranges as attributes
 # Output: The building you are looking at or False (if it doesn't collide with a building)
 def collision_check(position, orientation):
 	dictionary = parse('DA_WISE_GMLs/DA12_3D_Buildings_Merged.gml')
 	time = 0
 	s_x, s_y, s_z = position
-	theta, alpha, beta = orientation
+	theta, alpha = orientation
 	current_position = position
 
 	while within_model_bounds(current_position):
@@ -38,18 +37,26 @@ def collision_check(position, orientation):
 			return collision
 		else:
 			time += 1
-			new_x = s_x + time*math.cos(beta)
+			new_x = s_x + time*math.cos(theta)
 			new_y = s_y + time*math.sin(theta)
 			new_z = s_z + time*math.sin(alpha)
 			current_position = (new_x, new_y, new_z)
 	return False
 
 if __name__ == '__main__':
-	# Simple test that shows what happens if the starting point is in a building
+	
 	test_point = [*GPS_to_coordinate(40.713009, 74.013678),0.0]
-	coord = collision_check(test_point,[1.0,1.0,1.0])
+	coord = collision_check(test_point,[1.0,1.0])
 	assert coord == "Bldg_21510003972", "Cannot identify when intial point is in building"
 
 	test_point = [*GPS_to_coordinate(40.700678,74.017636),0.0]
-	coord = collision_check(test_point,[0.0,3.1415/2,3.1415/2])
+	coord = collision_check(test_point,[0.0,3.1415/2])
 	assert coord == False, "You aren't suppose to see anything if you look up in the Hudson"
+	
+	test_point = [*GPS_to_coordinate(40.712348,74.013641),1.0]
+	coord = collision_check(test_point,[math.pi/2,0.0])
+	assert coord == "Bldg_21510003972", "If you are standing in front of One WTC and look north, you should see it" 
+	
+	test_point = [*GPS_to_coordinate(40.748160,73.984741),40.0]
+	coord = collision_check(test_point,[math.pi,0.0])
+	assert coord == "Bldg_12210009096", "If you are standing in front of Empire State Building and look west, you should see it"
